2a65967813df48d376bbd6ecd9b357b97efbfe20
kartikdixit03 <kartikdixit37093@gmail.com>
2022-09-02 02:18:48 +0530

    2.1

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,30 +1,41 @@
 import React ,{ useState } from 'react';
 
-import Button from '@mui/material/Button';
 import "./Main.css";
 
 const Main = () => {
-  const [timeoutID, updateTimeoutID] = useState();
-  
+  const [number, setNumber] = useState(null)
+  const [dogImage, setImage] = useState('');
 
-  const fetchData = async (searchString) => {
-   
-  }
+  async function fetchImage() {
+    if (!number) {
+      alert('Please enter the number first then hit the button')
+      return
+    }
 
-  const onTextChange = (event) => {
-    clearTimeout(timeoutID);
-    //UpdateSearchQuery(event.target.value);
-    const timeout = setTimeout(() => fetchData(event.target.value), 500);
-    updateTimeoutID(timeout);
-    
+    if (number < 100 || number > 599) {
+      alert('Not a valid HTTP code')
+      return
+    }
+
+    const response = await fetch(`https://http.dog/${number}.jpg`).catch(() => null)
+    if (!response) {
+      alert('Something went wrong, please check the number provided')
+      return
+    }
+
+    const data = await response.blob();
+    setImage(URL.createObjectURL(data))
   };
+ 
     return(
         <div className="App">
       <div className="container">
         <header className="header">Get Your Dog
 
       
-     <button className="button-3" role="button">Follow US</button>
+      <a href='https://twitter.com/dog_feelings' target='_blank' >
+  <button className="button-3" role="button">Follow US</button>
+     </a>
 
 
 
@@ -34,21 +45,18 @@ const Main = () => {
         <p>Dogs for every HyperText Transfer Protocol response status code.</p>
         <div className='searchBox'>
           <div>
-            <input
+          <input
               className='searchInput'
               placeholder="               ENTER  YOUR  NUMBER  HERE"
-
-              onChange={onTextChange}
-              // value={}
-              
+              onChange={(event) => setNumber(event.target.value)}
               >
             </input>
-            <image src=''></image>
+           { dogImage.length > 0 && <image src={dogImage}></image> }
           </div>
           
         </div>
       </div>
     </div>
     )
-}
+};
 export default Main;
